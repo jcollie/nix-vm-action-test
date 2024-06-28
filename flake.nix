b421d6867a33b70e837ad36f64c8da29b8130a3f
Jeffrey C. Ollie <jeff@ocjtech.us>
2024-06-28 18:21:54 -0500

    first

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  inputs = {
+    nixpkgs-23-05 = {
+      url = "github:nixos/nixpkgs/nixos-23.05";
+    };
+    nixpkgs-23-11 = {
+      url = "github:nixos/nixpkgs/nixos-23.11";
+    };
+    nixpkgs-24-05 = {
+      url = "github:nixos/nixpkgs/nixos-24.05";
+    };
+    nixpkgs-unstable = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-unstable";
+        flake-compat.follows = "";
+      };
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs-unstable,
+    zig,
+    ...
+  } @ inputs:
+    builtins.foldl' nixpkgs-unstable.lib.recursiveUpdate {} (builtins.map (system: let
+      versions = ["23.05" "23.11" "24.05" "unstable"];
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+      };
+      ghostty-source = pkgs-unstable.stdenvNoCC.mkDerivation {
+        name = "ghostty-source";
+        src = ./.;
+        dontPatch = true;
+        dontConfgure = true;
+        dontBuild = true;
+        doTest = false;
+        installPhase = ''
+          cp -rv $src $out
+        '';
+      };
+      ghostty-sshkeys = pkgs-unstable.runCommand "ghostty-sshkeys" {} ''
+        mkdir -p $out
+        ${pkgs-unstable.openssh}/bin/ssh-keygen -C "" -N "" -t ed25519 -f $out/id_ed25519
+      '';
+    in {
+      overlay = final: prev: {
+        inherit ghostty-source ghostty-sshkeys;
+        zig = zig.packages.${prev.system}."0.13.0";
+      };
+      packages.${system} =
+        {
+          vm-ssh = pkgs-unstable.writeScriptBin "ssh" ''
+            ${pkgs-unstable.openssh}/bin/ssh -F none -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i ${ghostty-sshkeys}/id_ed25519 ssh://root@localhost:2222  "$@"
+          '';
+        }
+        // (
+          builtins.listToAttrs
+          (
+            builtins.map (
+              version: let
+                v = builtins.replaceStrings ["."] ["-"] version;
+              in {
+                name = "vm-${v}";
+                value = self.nixosConfigurations."vm-${v}-${system}".config.system.build.vm;
+              }
+            )
+            versions
+          )
+        );
+      nixosConfigurations =
+        builtins.listToAttrs
+        (
+          builtins.map (
+            version: let
+              v = builtins.replaceStrings ["."] ["-"] version;
+            in {
+              name = "vm-${v}-${system}";
+              value = inputs."nixpkgs-${v}".lib.nixosSystem {
+                specialArgs = {
+                  stateVersion = version;
+                  hostPlatform = system;
+                  flake = self;
+                };
+                modules = [./vm.nix];
+              };
+            }
+          )
+          versions
+        );
+    }) (builtins.attrNames zig.packages));
+}
---
+++ vm.nix
@@ -0,0 +1,76 @@
+{
+  flake,
+  modulesPath,
+  stateVersion,
+  hostPlatform,
+  pkgs,
+  ...
+}: {
+  imports = ["${modulesPath}/virtualisation/qemu-vm.nix"];
+  # These values are tuned such that the VM performs on Github Actions runners.
+  virtualisation = {
+    forwardPorts = [
+      {
+        from = "host";
+        host.port = 2222;
+        guest.port = 22;
+      }
+    ];
+    sharedDirectories = {
+      workspace = {
+        source = "$GITHUB_WORKSPACE";
+        target = "/mnt";
+      };
+    };
+    cores = 2;
+    memorySize = 5120;
+    diskSize = 10240;
+  };
+
+  system.stateVersion = stateVersion;
+  nixpkgs.hostPlatform = hostPlatform;
+  nixpkgs.overlays = [flake.outputs.overlay];
+
+  nix = {
+    settings = {
+      substituters = [
+        "https://ghostty.cachix.org/"
+      ];
+      trusted-public-keys = [
+        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
+      ];
+    };
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  # Root user without password and enabled SSH for playing around
+  networking.firewall.enable = false;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+      KbdInteractiveAuthentication = false;
+      PasswordAuthentication = false;
+    };
+  };
+
+  users.users = {
+    root = {
+      password = "password";
+      openssh = {
+        authorizedKeys = {
+          keyFiles = [
+            "${pkgs.ghostty-sshkeys}/id_ed25519.pub"
+          ];
+        };
+      };
+    };
+  };
+
+  environment.systemPackages = [
+    pkgs.git
+    pkgs.zig
+  ];
+}
